63ecd43fb2949245469162635cda051291c6657f
dmiloloza <domagoj.miloloza@gmil.com>
2021-01-30 17:53:48 +0100

    hex tab completed

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -4,12 +4,11 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 const changeColor = () => {
-    //Math.random()* colors.length => because color array is 4 (math.random 0-1 without 1)
-    const randomNumber = Math.floor(Math.random() * colors.length);
+  //Math.random()* colors.length => because color array is 4 (math.random 0-1 without 1)
+  const randomNumber = Math.floor(Math.random() * colors.length);
 
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
 };
 
 const buttonClick = btn.addEventListener('click', changeColor);
-
