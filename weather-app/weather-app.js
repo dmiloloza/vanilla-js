140bab4a3a41a3f4837e404f60b89b75081dbfbf
dmiloloza <domagoj.miloloza@gmil.com>
2021-01-31 19:08:23 +0100

    icon working

--- weather-app/weather-app.js
+++ weather-app/weather-app.js
@@ -1,4 +1,4 @@
-// import {apiKey} from "./api";
+import apiKey from "./api.js";
 
 window.addEventListener('load', () => {
   let latitude;
@@ -8,29 +8,23 @@ window.addEventListener('load', () => {
   let temperatureDegree = document.querySelector('.temperature-degree');
   let temperatureDescription = document.querySelector('.temperature-description');
 
-  //TODO
-  let icon = document.querySelector('image');
-
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(location => {
       latitude = location.coords.latitude;
       longitude = location.coords.longitude;
 
-      fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey} &q=${latitude},${longitude}`)
+      fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}`)
         .then(response => {
           return response.json();
         })
         .then(weatherData => {
-          console.log(weatherData);
           const {temp_c, condition} = weatherData.current;
           //set DOM elements from the API
           locationTimezone.textContent = weatherData.location.name;
           temperatureDegree.textContent = temp_c;
           temperatureDescription.textContent = condition.text;
           //set icon
-          //TODO
-          icon.setAttribute() = condition.icon
-          console.log(icon);
+          document.getElementById('icon').src= condition.icon
         }).catch(err => {
         console.error(err)
       })
