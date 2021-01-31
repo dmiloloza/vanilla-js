fa24928e2ca88007b0276a9b065dba4e364f2858
dmiloloza <domagoj.miloloza@gmil.com>
2021-01-31 18:26:11 +0100

    icons

--- weather-app/weather-app.js
+++ weather-app/weather-app.js
@@ -1,6 +1,5 @@
 // import {apiKey} from "./api";
 
-//https://darkskyapp.github.io/skycons/
 window.addEventListener('load', () => {
   let latitude;
   let longitude;
@@ -9,21 +8,29 @@ window.addEventListener('load', () => {
   let temperatureDegree = document.querySelector('.temperature-degree');
   let temperatureDescription = document.querySelector('.temperature-description');
 
+  //TODO
+  let icon = document.querySelector('image');
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(location => {
       latitude = location.coords.latitude;
       longitude = location.coords.longitude;
 
-      fetch(`http://api.weatherapi.com/v1/current.json?key=34da07b1761b47f7a93101023213101&q=${latitude},${longitude}`)
+      fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey} &q=${latitude},${longitude}`)
         .then(response => {
           return response.json();
         })
         .then(weatherData => {
+          console.log(weatherData);
           const {temp_c, condition} = weatherData.current;
           //set DOM elements from the API
           locationTimezone.textContent = weatherData.location.name;
           temperatureDegree.textContent = temp_c;
           temperatureDescription.textContent = condition.text;
+          //set icon
+          //TODO
+          icon.setAttribute() = condition.icon
+          console.log(icon);
         }).catch(err => {
         console.error(err)
       })
