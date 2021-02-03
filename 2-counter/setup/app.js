3764cc07491c27ce3b3c3dd57c036c0ed9294f41
dmiloloza <domagoj.miloloza@gmil.com>
2021-02-03 12:29:55 +0100

    button selector

---
+++ 2-counter/setup/app.js
@@ -0,0 +1,6 @@
+// set initial count
+let count = 0;
+
+//select value and buttons
+const value = document.querySelector('#value') //# is id
+const buttons = document.querySelectorAll('.btn') // all button with class of btn...
