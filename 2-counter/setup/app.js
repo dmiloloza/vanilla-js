1e092c5028e19c423ff97734927144156638748e
dmiloloza <domagoj.miloloza@gmil.com>
2021-02-03 12:30:40 +0100

    event listener for each button

--- 2-counter/setup/app.js
+++ 2-counter/setup/app.js
@@ -4,3 +4,20 @@ let count = 0;
 //select value and buttons
 const value = document.querySelector('#value') //# is id
 const buttons = document.querySelectorAll('.btn') // all button with class of btn...
+
+//buttons is Node list, some of array methods can be used.
+//for each item from node list adding event listener
+buttons.forEach(button => {
+  button.addEventListener('click', e => {
+    const styles = e.currentTarget.classList;
+
+    if (styles.contains('decrease')) {
+      count--;
+    } else if (styles.contains('increase')) {
+      count++;
+    } else {
+      count = 0;
+    }
+    value.textContent = count
+  });
+});
