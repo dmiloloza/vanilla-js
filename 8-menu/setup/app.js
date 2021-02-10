cb5f20f56aa6ff8c18599b5dd0c04620b6e81388
dmiloloza <domagoj.miloloza@gmil.com>
2021-02-10 14:06:48 +0100

    (script) display menu items, and menu buttons close #56

--- 8-menu/setup/app.js
+++ 8-menu/setup/app.js
@@ -70,13 +70,27 @@ const menu = [
     price: 16.99,
     img: './images/item-9.jpeg',
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
+  },
+  {
+    id: 10,
+    title: 'steak dinner',
+    category: 'dinner',
+    price: 39.99,
+    img: './images/item-10.jpeg',
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
   }
 ];
-
+// get parent element
 const sectionCenter = document.querySelector('.section-center');
-
+const btnContainer = document.querySelector('.btn-container');
+// display all items when page loads
 window.addEventListener('load', () => {
-  let displayMenu = menu.map(item => {
+  displayMenuItems(menu);
+  displayMenuButtons();
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(item => {
     return `<article class='menu-item'>
               <img src='${item.img}' class='photo' alt='menu item'>
               <div class='item-info'>
@@ -88,6 +102,44 @@ window.addEventListener('load', () => {
               </div>
             </article>`;
   });
+
   displayMenu = displayMenu.join('');
   sectionCenter.innerHTML = displayMenu;
-});
+}
+
+function displayMenuButtons() {
+  const categories = menu.reduce((values, item) => {
+
+    if (!values.includes(item.category)) {
+      values.push(item.category);
+    }
+    return values;
+  }, ['all']);
+
+  let categoryBtns = categories.map((category) => {
+    return `<button class='filter-btn' type='button' data-id='${category}'>${category}</button'>`;
+  });
+
+  categoryBtns = categoryBtns.join('');
+  btnContainer.innerHTML = categoryBtns;
+  const filterBtns = document.querySelectorAll('.filter-btn');
+
+  //filter items
+  filterBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(menuItem => {
+
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+
+      if (category === 'all') {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
+}
